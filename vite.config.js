39cea737cc0d2253a2e6c09d97fbeb847108aed2
AgusT613 <torresagustin613@gmail.com>
2023-08-06 17:08:13 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/https://github.com/AgusT613/tic-tac-toe_react_app/'
+  base: 'tic-tac-toe_react_app'
 })
